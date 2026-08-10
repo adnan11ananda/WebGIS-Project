7cdc08561418b54ea08f68b689288ee770c0b7f3
adnan11ananda <adnan.ananda.1992@gmail.com>
2026-08-11 05:31:07 +0700

    add geojson file as a layer to webmap

--- project-webgis/src/main.js
+++ project-webgis/src/main.js
@@ -1,4 +1,6 @@
 import { Map } from 'maplibre-gl';
+import JalanPbm from "./data/Way_Prabumulih.geojson?url";
+import layanan_kesehatan from "./data/Layanan_Kesehatan_Pbm.geojson?url";
 
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
@@ -10,4 +12,37 @@ const map = new Map({
   style: 'https://demotiles.maplibre.org/style.json',
   center: [104.23117780290508, -3.441584955294986],
   zoom: 7
+});
+
+map.on('load', () => {
+  map.addSource('jalan.pbm', {
+    type: 'geojson',
+    data: JalanPbm
+  });
+
+  map.addSource('layanan_kesehatan', {
+    type: 'geojson',
+    data: layanan_kesehatan
+  });
+
+  
+
+  map.addLayer({
+    id: 'jalan-layer',
+    type: 'line',
+    source: 'jalan.pbm',
+    paint: {
+      'line-color': '#FF0000',
+      'line-width': 3
+    }
+  });
+  map.addLayer({
+    id: 'layanan_kesehatan-layer',
+    type: 'circle',
+    source: 'layanan_kesehatan',
+    paint: {
+      'circle-radius': 5,
+      'circle-color': '#0000FF'
+    }
+  });
 });
