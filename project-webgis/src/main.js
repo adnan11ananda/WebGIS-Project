fa6ab558e3868fcb141bda40372aa7db58faa26d
adnan11ananda <adnan.ananda.1992@gmail.com>
2026-08-16 22:43:51 +0700

    add atribution control

--- project-webgis/src/main.js
+++ project-webgis/src/main.js
@@ -1,4 +1,7 @@
-import { Map } from 'maplibre-gl';
+import { 
+  Map, 
+  AttributionControl } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
 import { addAreaLayer } from './layers/Area.js';
 import { addLineLayer } from './layers/line.js';
 import { addPointLayer } from './layers/point.js';
@@ -12,12 +15,17 @@ const map = new Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/style.json',
   center: [104.23117780290508, -3.441584955294986],
-  zoom: 10
+  zoom: 10,
+ attributionControl: false
 });
 
 map.on('load', () => {
   addAreaLayer(map);
   addLineLayer(map);
   addPointLayer(map);
-  
   });
+
+  map.addControl(new AttributionControl({
+  compact: true,
+  customAttribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors | &copy; <a href="https://tanahair.indonesia.go.id/portal-web/">2026 Badan Informasi Geospasial</a>'
+}));
--- project-webgis/src/pages/kec-prabu.js
+++ project-webgis/src/pages/kec-prabu.js
@@ -1,7 +1,10 @@
-import { Map } from 'maplibre-gl';
-import { addAreaLayer } from '../../src/layers/Area.js';
-import { addLineLayer } from '../../src/layers/line.js';
-import { addPointLayer } from '../../src/layers/point.js';
+import { 
+  Map,
+AttributionControl } from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import { addAreaLayer } from '../layers/Area.js';
+import { addLineLayer } from '../layers/line.js';
+import { addPointLayer } from '../layers/point.js';
 
 const mapElement = document.createElement('div');
 mapElement.id = 'map';
@@ -12,11 +15,15 @@ const map = new Map({
   container: 'map',
   style: 'https://demotiles.maplibre.org/style.json',
   center: [104.23117780290508, -3.441584955294986],
-  zoom: 10
+  zoom: 10,
+  attributionControl: false
 });
 
 map.on('load', () => {
   addAreaLayer(map);
-  
-  
   });
+
+map.addControl(new AttributionControl({
+  compact: true,
+  customAttribution: '&copy; <a href="https://tanahair.indonesia.go.id/portal-web/">2026 Badan Informasi Geospasial</a>'
+}));
